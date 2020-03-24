a45a72052ab8143928a2d5e841a17e3d4622b489
matthew enbar <matthew.enbar@gmail.com>
2020-03-24 18:56:27 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,6 +2,8 @@ def square_array(array)
   new_array = []
   array.each {|x| new_array << x ** 2 }
   # the double * is to the power of
-  
+  # << is adding new element to the bottom (end) of the new array
+  # << is the short version of .push
+
   return new_array
 end
