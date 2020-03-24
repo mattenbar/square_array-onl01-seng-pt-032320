51d1b202f36cd94a60c7d0b010a942bdf7235d2a
matthew enbar <matthew.enbar@gmail.com>
2020-03-24 15:44:56 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,7 @@
 def square_array(array)
-  # your code here
+  new_array = []
+  array.each {|x| new_array << x ** 2 }
+  # the double * is to the power of
+  # x is each location in the original array
+  return new_array
 end
